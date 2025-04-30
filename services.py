9bd4d1bf5967739ff3e9077f0447d24f8229a7bd
miv403 <miv403@duck.com>
2025-04-30 22:09:47 +0300

    mDNS servis keşfi modülü eklendi
    mDNS DNS kaydı ve istenen DNS kaydının güncel IP adresini
    bulan arayüz eklendi

---
+++ services.py
@@ -0,0 +1,119 @@
+from zeroconf import ServiceInfo, Zeroconf, ServiceBrowser, ServiceListener
+import socket
+import time
+import sys
+
+DEBUG = "--debug" in sys.argv
+
+class Listener(ServiceListener):
+
+    def __init__(self, _id):
+        self.ID = _id
+        self.foundServices = []
+    
+    def update_service(self, zc, type_, name):
+        print(f"[SERVICE LISTENER] {name} updated")
+    def remove_service(self, zc, type_, name): # program akışında çalışmıyor
+        #if self.foundServices and self.foundServices['name'] == name:
+        print(f"[SERVICE LISTENER] {name} was requested to be removed [FIX] {__name__}:{sys._getframe(1).f_lineno}")
+
+    def add_service(self, zc, type_, name):
+        info = zc.get_service_info(type_, name)
+        print(f"[SERVICE LISTENER] Service {name} added")
+        
+        if info:
+            if DEBUG:
+                addresses = ["%s" % socket.inet_ntoa(addr) 
+                                for addr in info.addresses]
+                print(f"[DEBUG] Found service")
+                print(f"\tname: {name}")
+                print(f"\ttype: {type_}")
+                print(f"\taddresses: {addresses}")
+                print(f"\tport: {info.port}")
+                print(f"\tserver: {info.server}")
+
+            if info.server.split('.')[0] == self.ID:
+                # istenen ID eşleşirse bulunan servislere ekleniyor
+                self.foundServices.append( {
+                    'name' : name,
+                    'type' : type_,
+                    'addresses' : addresses,
+                    'port' : info.port,
+                    'server' : info.server
+                })
+                print(f"[SERVICE LISTENER] Found service: {name} @ {addresses}")
+
+class Service:
+    
+    def __init__(self, _id, port, addr):
+        self.listener = Listener()
+        self.ID = _id
+        self.PORT = port
+        self.LOCALHOST = addr
+        self.APP_NAME = "desit"
+        self.serviceType = "_{self.APP_NAME}._tcp.local."
+
+class ServiceRegister(Service):
+    
+    def register(self): # mDNS servis kaydı
+        self.serviceName = self.ID
+
+        serviceInfo = ServiceInfo(
+            type_ = self.serviceType,
+            name = f"{self.serviceName}.{self.serviceType}",
+            addresses = [socket.inet_aton(self.LOCALHOST)],
+            port = self.PORT,
+            server = f"{self.serviceName}.local",
+            properties = {}
+        )
+        
+        zc = Zeroconf()
+        print(f"[SERVICE] registering {self.serviceName} @ {self.LOCALHOST}:{self.PORT}")
+        zc.register_service(self.serviceInfo)
+        
+        try:
+            while True:
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            zc.unregister_service(ServiceInfo)
+            zc.close()
+            print(f"[SERVICE] {self.serviceName} unregistered")
+
+class ServiceDiscover(Service):
+
+    def discoverService(self, timeout = 5):
+        
+        # istenen ID (_id) için servis araması yapar.
+        # bulunan servisin güncel IP adresi geri döndürülür.
+        
+        zc = Zeroconf()
+        listener = Listener(self.ID)
+        browser = ServiceBrowser(zc, self.serviceType, listener)
+        
+        print(f"[DISCOVER] searching {self.ID} with {self.serviceType} for {timeout} secs ")
+
+        time.sleep(timeout)
+        zc.close()
+        
+        if DEBUG:
+            self.servicePrint(listener.foundServices)
+            print(f"[DEBUG] return {listener.foundServices[0]['addresses'][0]}")
+
+        return listener.foundServices[0]['addresses'][0] # bulunan ID için IP adresi döndürülür
+
+    def servicePrint(self, service):
+        print(f"[DEBUG]")
+        if  service != []:
+            for s in service:
+                    print("\nDiscovered service details:")
+                    print(f"Name: {s['name']}") # my-s.myapp._tcp.local.
+                    print(f"Type: {s['type']}")
+                    print(f"IP Addresses: {', '.join(s['addresses'])}")
+                    print(f"Port: {s['port']}")
+                    print(f"Server: {s['server']}") # my-s.local.
+                    
+                    print(f"Device ID: {s['server'].split('.')[0]}")
+        else:
+            print("[DEBUG] No service found")
